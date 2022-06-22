41c1eb5f624e57c15c6a4ca46116682d43124515
Devonjedi <devon.jedi@gmail.com>
2022-06-22 10:37:16 -0500

    updated name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
     // Site URL for when it goes live
-    siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
+    siteUrl: `devonek.com`,
     // Your Name
-    name: 'Ryan Fitzgerald',
+    name: 'Devon Ek',
     // Main Site Title
-    title: `Ryan Fitzgerald | Full-Stack Developer`,
+    title: `Devon Ek | Drupal Developer`,
     // Description that goes under your name in main bio
     description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
     // Optional: Twitter account handle
